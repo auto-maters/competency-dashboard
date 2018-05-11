b71cedf12cc2fb2b45e21526d1069a90f8cd3e68
Ashutosh Sharma - Network <sharma_ashutosh@network.lilly.com>
2018-05-11 20:49:52 +0530

    Updating code for dashboard page

--- src/public/javascripts/dashboard-controller.js
+++ src/public/javascripts/dashboard-controller.js
@@ -8,13 +8,79 @@ coreApp.controller('dashboardController', ['$scope', '$q', '$timeout', 'Competen
   $scope.dashboardData = [];
 
   function fillDataToDashboard() {
-    $scope.nodeJS = {};
+    $scope.nodeJS = [];
+    $scope.angularJS = [];
+    $scope.htmlCSS = [];
+    $scope.rubyOnRails = [];
+    $scope.cloudComputingGeneral = [];
+    $scope.internetOfThings = [];
+    $scope.bigDataHadoop = [];
+    $scope.mobileAndroid = [];
+    $scope.tableauBI = [];
+    $scope.wex = [];
 
-    for (const comp in $scope.dashboardData.data.records) { // eslint-disable-line
+    $scope.dashboardData.data.records.forEach((comp) => {
       if (comp.emp_comp_name === 'Digital : Node.js') {
-        $scope.nodeJS[comp.emp_comp_level] = comp.comp_level_count;
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.nodeJS.push(obj);
       }
-    }
+      if (comp.emp_comp_name === 'Digital : AngularJS') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.angularJS.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : HTML5-CSS3') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.htmlCSS.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : Ruby on Rails') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.rubyOnRails.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : Cloud Computing (General)') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.cloudComputingGeneral.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : Internet of Things') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.internetOfThings.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : BigData and Hadoop Ecosystems') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.bigDataHadoop.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : Mobile Computing - Android') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.mobileAndroid.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : BI Data Visualization - Tableau') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.tableauBI.push(obj);
+      }
+      if (comp.emp_comp_name === 'Digital : IBM Watson Explorer') {
+        const obj = {};
+        obj.level = comp.emp_comp_level;
+        obj.level_count = comp.comp_level_count;
+        $scope.wex.push(obj);
+      }
+    });
   }
 
   function initializeData() {
